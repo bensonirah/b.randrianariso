6869bf4395a69659e1136ba0fcfeb03f0b364089
bensonirah <bensonirah@gmail.com>
2022-09-04 08:56:45 +0300

    update uninstall script

--- uninstall.sh
+++ uninstall.sh
@@ -2,6 +2,10 @@
 set -e
 echo "I'm trying to uninstall the docker image of this project"
 echo "Please wait!"
+docker container ls |grep bensonirah |awk -F' ' '{print $1}' |xargs docker container stop
 docker image rm bensonirah/bensonirah-io:latest
 echo "Image successful removed"
-echo "Finally remove the project root manually"
+echo "Trying to remove the project root.."
+cd ../
+rm -rf bensonirah_portfolio
+echo "Project removed success full"
